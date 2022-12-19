ac186eb17ef03a2ae8a77c4657260763a63c91c5
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-12-19 21:52:08 +0300

    project created

---
+++ components/homepage/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Homepage() {
+  return <div className="w-10 h-10 bg-black"> burası</div>;
+}
+
+export default Homepage;
---
+++ pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Homepage from "../../components/homepage/Homepage";
+const Index = ({ categoryList, productList }) => {
+  return (
+    <React.Fragment>
+      <Homepage />
+    </React.Fragment>
+  );
+};
+
+export default Index;
